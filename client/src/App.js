0da0b52e6072386fcd8b46962ffaa236dd5815e3
ggyu <kcpooh0202@naver.com>
2020-11-24 23:55:44 +0900

    노드리액트

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/RegisterPage" component={RegisterPage} />
+          <Route exact path="/LoginPage" component={LoginPage} />
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
